991cf603eed7b51c6b92803ade517998e988845c
Cristian Lungu <cristian.lungu@hsrw.org>
2020-03-30 17:29:23 +0200

    fixed url

--- src/config.ts
+++ src/config.ts
@@ -17,5 +17,6 @@ export enum InteractionType {
 export const joloColor = 'rgb(148, 47, 81)'
 export const selectColor = '#f3c61c'
 
-export const backendUrl = 'http://192.168.0.3:9000'
+// TODO replace with SERVICE_URL
+export const backendUrl = 'http://localhost:9000'
 // export const backendUrl = 'https://botm-pool-demo.jolocom.com'
